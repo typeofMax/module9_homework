5e9a3a0563f133d4e2d1e4d39706b772f87593f4
typeofMax <work@typeofmax.ru>
2023-03-28 15:43:03 +0300

    Complete task 2

---
+++ task_2.js
@@ -0,0 +1,19 @@
+const jsonString = `
+{
+  "list": [
+    {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+    },
+    {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+    }
+  ]
+}`;
+
+const result = JSON.parse(jsonString);
+
+console.log(result);
